8296203e05933145e18ff5766223e7f75aff3d4b
Bogere Siraje <bogeresiraje25@gmail.com>
2020-12-23 09:06:38 +0300

    Create front skeleton.

--- front/views.py
+++ front/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from business.models import Business
+
+
+def index(request):
+    businesses = Business.objects.all()[:3]
+    context = { 'businesses': businesses }
+    return render(request, 'front/base.html', context)
